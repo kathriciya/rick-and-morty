d0c517cd535af7c5e540afae4e8b35e050de48d2
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-05 18:03:43 +0700

    create config

---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import cn from 'classnames';
+import s from './app.module.scss';
+
+const App = () => {
+  return (
+    <div className={cn('container', s.app)}>
+      <h1>kjhdfkjghkfdhg</h1>
+    </div>
+  );
+};
+export default App;
