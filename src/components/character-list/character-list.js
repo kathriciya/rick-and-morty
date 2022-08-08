0d4ce2a59a77fe002169320fcfa09c448dc5b626
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-08 20:59:22 +0700

    create CharacterList

---
+++ src/components/character-list/character-list.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import cn from 'classnames';
+import s from './character-list.module.scss';
+import Character from '../character-item/character-item';
+
+const CharacterList = ({ episode }) => {
+  const elements = episode.characters.map((item, i) => (
+    <Character key={i} linkToCharacter={item} />
+  ));
+  // console.log('elementsdd: ', elements);
+  return (
+    <div className={s.character_list_wrap}>
+      <ul className={cn('list-group', s.list)}>{elements}</ul>
+    </div>
+  );
+};
+
+export default CharacterList;
