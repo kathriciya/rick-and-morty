0983ec330336fc53f2f13822a0ced1db0cc5ce3f
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-09 06:47:06 +0700

    create LocationAllPage

---
+++ src/components/pages/location-all-page/location-all-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import s from './location-all-page.module.scss';
+import Layout from '../../layout/layout';
+
+const LocationAllPage = () => (
+  <Layout title='LocationAllPage'>
+    <span>fghgfjhjh</span>
+  </Layout>
+);
+
+export default LocationAllPage;
