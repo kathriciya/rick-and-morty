28503835e9d24730db27bf9ecea896d3288d231d
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-09 06:46:49 +0700

    create CharacterAllPage

---
+++ src/components/pages/character-all-page/character-all-page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import s from './character-all-page.module.scss';
+import Layout from '../../layout/layout';
+
+const CharacterAllPage = () => (
+  <Layout title='CharacterAllPage'>
+    <span>fghgfjhjh</span>
+  </Layout>
+);
+
+export default CharacterAllPage;
