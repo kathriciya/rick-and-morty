4472207e14d9dd72a36ce7074814f04679fba262
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-08 20:55:58 +0700

    update SearchPanel

--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -5,13 +5,13 @@ import s from './search-panel.module.scss';
 const SearchPanel = ({ onUpdateSearch }) => {
   const [term, setTerm] = useState('');
   const onChangeValue = (e) => {
-    const term = e.target.value;
-    setTerm(term);
-    onUpdateSearch(term);
+    const val = e.target.value;
+    setTerm(val);
+    onUpdateSearch(val);
   };
   return (
     <div className={s.search_panel}>
-      <i className={cn('fas fa-magnifying-glass', s.search_icon)}></i>
+      <i className={cn('fas fa-magnifying-glass', s.search_icon)} />
       <input
         type='text'
         className={cn('form-control', s.input)}
