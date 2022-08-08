5b196b1fdad9a6c03097162a450a48a36a1e6e5c
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-09 06:48:02 +0700

    add navigation

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,42 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import cn from 'classnames';
 import s from './header.module.scss';
 
-const Header = () => (
-  <header className={s.header}>
-    <div className={s.wrapper}>
-      <ul className={cn('d-flex', s.list)}>
-        <li className={s.item}>Episodes</li>
-        <li className={s.item}>Locations</li>
-        <li className={s.item}>Characters</li>
-      </ul>
-    </div>
-  </header>
-);
+const Header = () => {
+  const setActiveLink = ({ isActive }) =>
+    isActive ? `${s.link} ${s.active}` : s.link;
+  return (
+    <header className={s.header}>
+      <div className={s.wrapper}>
+        <nav>
+          <ul className={cn('d-flex', s.list)}>
+            <li className={s.item}>
+              <NavLink to='/' className={setActiveLink}>
+                Main
+              </NavLink>
+            </li>
+            <li className={s.item}>
+              <NavLink to='/episode' className={setActiveLink}>
+                Episodes
+              </NavLink>
+            </li>
+
+            <li className={s.item}>
+              <NavLink to='/location' className={setActiveLink}>
+                Locations
+              </NavLink>
+            </li>
+            <li className={s.item}>
+              <NavLink to='/character' className={setActiveLink}>
+                Characters
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
