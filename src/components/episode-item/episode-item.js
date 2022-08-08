05181c7b787ed78343306f2c877d37634e86d1d4
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-08 20:57:54 +0700

    update EpisodeItem

--- src/components/episode-item/episode-item.js
+++ src/components/episode-item/episode-item.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import cn from 'classnames';
 import s from './episode-item.module.scss';
 
-const EpisodeItem = ({ episode, title, date }) => {
+const EpisodeItem = ({ episode, title, date, url }) => {
+  const numberId = url.match(/\d+$/);
+  const numberEpisode = numberId[0];
   const number = episode.slice(-2).replace(/^0/, '');
 
   return (
@@ -12,9 +15,21 @@ const EpisodeItem = ({ episode, title, date }) => {
         s.episode_item
       )}
     >
-      <span className={s.episode}>Episode {number}</span>
-      <span className={s.title}>{title}</span>
-      <span className={s.date}>{date}</span>
+      <Link className={s.link} to={episode}>
+        <div className='d-flex align-items-baseline'>
+          <span className={s.number}>{number}.</span>
+          <span className={s.title}>{title}</span>
+        </div>
+        <div
+          className={cn(
+            'd-flex justify-content-between align-items-baseline',
+            s.episode_inner
+          )}
+        >
+          <span className={s.episode}>Episode {numberEpisode}</span>
+          <span className={s.date}>{date}</span>
+        </div>
+      </Link>
     </li>
   );
 };
