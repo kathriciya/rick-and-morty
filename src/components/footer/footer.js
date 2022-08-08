961c9aea359fb29198e63eceaca21090657c607f
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-09 01:43:43 +0700

    create Footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import s from './footer.module.scss';
+
+const Footer = () => (
+  <footer className={s.footer}>
+    <div className={s.wrapper}>
+      <span>THANKS FOR VISITING</span>
+      <span>© 2022</span>
+    </div>
+  </footer>
+);
+
+export default Footer;
