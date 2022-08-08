c1ea04e8d93585def74c24975d3b007f868a47d3
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-08 20:57:25 +0700

    update EpisodeList

--- src/components/episode-list/episode-list.js
+++ src/components/episode-list/episode-list.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import EpisodeItem from '../episode-item/episode-item';
 import cn from 'classnames';
+import EpisodeItem from '../episode-item/episode-item';
+
 import s from './episode-list.module.scss';
 
 const EpisodeList = ({ episodes }) => {
-  const elements = episodes.map((item) => {
-    return (
-      <EpisodeItem
-        key={item.id}
-        date={item.air_date}
-        title={item.name}
-        episode={item.episode}
-      />
-    );
-  });
+  // console.log('episodes : ', episodes);
+  const elements = episodes.map((item) => (
+    <EpisodeItem
+      key={item.id}
+      date={item.air_date}
+      title={item.name}
+      episode={item.episode}
+      url={item.url}
+    />
+  ));
   return (
     <div className={s.episode_list_wrap}>
       <ul className={cn('list-group', s.list)}>{elements}</ul>
