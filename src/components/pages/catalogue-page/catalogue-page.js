6f4808c5834eb207917c13bc1ece827de77ba774
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-12 21:43:03 +0700

    add logic

--- src/components/pages/catalogue-page/catalogue-page.js
+++ src/components/pages/catalogue-page/catalogue-page.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import SearchPanel from '../../search-panel/search-panel';
 import SeasonFilter from '../../season-filter/season-filter';
 import EpisodeList from '../../episode-list/episode-list';
+import { getEpisodesAsync } from '../../../store/episodes';
+import api from '../../../api/api';
 import s from './catalogue-page.module.scss';
 import Layout from '../../layout/layout';
 
-const CataloguePage = (props) => {
-  const { onUpdateSearch, filter, onFilterSelect, visibleSeason } = props;
+const CataloguePage = () => {
+  const [episodes, setEpisodes] = useState([]);
+  const dispatch = useDispatch();
+  const [filter, setFilter] = useState('S01');
+  const [term, setTerm] = useState('');
+
+  useEffect(() => {
+    async function fetchData() {
+      const episodesRequest = await api.getAllEpisodes();
+      setEpisodes(episodesRequest);
+      dispatch(getEpisodesAsync(episodesRequest));
+    }
+    fetchData();
+  }, [dispatch]);
+
+  const filterPost = (items, filterType) =>
+    items.filter((item) => item.episode.includes(filterType));
+
+  const onFilterSelect = (filterType) => {
+    setFilter(filterType);
+  };
+
+  const searchEmp = (items, character) => {
+    if (character.length === 0) {
+      return items;
+    }
+
+    const value = character.toLowerCase();
+    return items.filter((item) => item.name.toLowerCase().startsWith(value));
+  };
+
+  const onUpdateSearch = (character) => {
+    setTerm(character);
+  };
+
+  const visibleSeason = filterPost(searchEmp(episodes, term), filter);
   return (
     <Layout title='Rick and Morty episodes catalogue'>
       <div className={s.app_panel}>
