e66f6dcd3c7f654ecf956fdd726121900f00f42a
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-12 21:45:40 +0700

    get episodes from store

--- src/components/pages/episode-page/episode-page.js
+++ src/components/pages/episode-page/episode-page.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import { selectEpisodesData } from '../../../store/episodes';
 import CharacterList from '../../character-list/character-list';
 import EpisodeInfo from '../../episode-info/episode-info';
 import Layout from '../../layout/layout';
 
-const EpisodePage = ({ visibleSeason }) => {
+const EpisodePage = () => {
+  const episodesRedux = useSelector(selectEpisodesData);
   const { episodeId } = useParams();
-  const episode = visibleSeason.find((item) => item.id === Number(episodeId));
+  const episode = episodesRedux.find((item) => item.id === Number(episodeId));
   return (
     <Layout title='Episode information'>
       <EpisodeInfo episode={episode} />
