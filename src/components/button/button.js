32769efe7779aea236f2373b620eb0e37d2109f2
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-17 15:16:30 +0700

    add props "disabled" for button

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,9 +2,14 @@ import React from 'react';
 import cn from 'classnames';
 import s from './button.module.scss';
 
-const Button = ({ label, onClick, className }) => {
+const Button = ({ label, onClick, className, disabled = false }) => {
   return (
-    <button className={cn(s.button, className)} type='button' onClick={onClick}>
+    <button
+      className={cn(s.button, className)}
+      type='button'
+      onClick={onClick}
+      disabled={disabled}
+    >
       {label}
     </button>
   );
