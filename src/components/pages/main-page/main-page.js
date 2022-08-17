07100886f8216e5439c1d3631e795b96d1a036de
Ekaterina <Kata_Yurevna@mail.ru>
2022-08-17 16:53:38 +0700

    add animation for MainPage

--- src/components/pages/main-page/main-page.js
+++ src/components/pages/main-page/main-page.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 // import cn from 'classnames';
-// import s from './main-page.module.scss';
+import s from './main-page.module.scss';
 import Layout from '../../layout/layout';
 import Button from '../../button/button';
 
@@ -17,7 +17,10 @@ const MainPage = () => {
       desc='Rick is a mad scientist who drags his grandson, Morty, on wild and crazy
         sci-fi adventures.'
     >
-      <Button label='Watch episodes' onClick={handleClick} />
+      <Button className={s.btn} label='Watch episodes' onClick={handleClick} />
+      <div className={s.box}>
+        <div className={s.heros} />
+      </div>
     </Layout>
   );
 };
